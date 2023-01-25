fc02c5b1e872d0b55d7eecb8baf2293910f6aa79
CrazyCoding330 <97684719+TopTen1004@users.noreply.github.com>
2023-01-24 21:04:43 -0600

    update global

--- class.rb
+++ class.rb
@@ -3,6 +3,7 @@ BEGIN {
  }
 $global_variable = 10
 class Customer
+    @@no_of_customers = 0
    def initialize(id, name, addr)
       @cust_id = id
       @cust_name = name
@@ -17,10 +18,23 @@ class Customer
       puts "Customer name #@cust_name"
       puts "Customer address #@cust_addr"
    end
+
+    def total_no_of_customers()
+        @@no_of_customers += 1
+        puts "Total number of customers: #@@no_of_customers"
+    end
 end
 
 object = Customer.new("1", "David", "Texas")
 object.display_details
+
+# Create Objects
+cust1 = Customer.new("1", "John", "Wisdom Apartments, Ludhiya")
+cust2 = Customer.new("2", "Poul", "New Empire road, Khandala")
+
+# Call Methods
+cust1.total_no_of_customers()
+cust2.total_no_of_customers()
 
 object.display_global
 END {
